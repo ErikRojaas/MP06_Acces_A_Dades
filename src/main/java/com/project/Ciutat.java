34e935daa709ee669e22548174c9a39401fd71a4
ErikRojaas <152431347+ErikRojaas@users.noreply.github.com>
2024-12-05 13:27:57 +0100

    Ejercicio 0 terminado

---
+++ src/main/java/com/project/Ciutat.java
@@ -0,0 +1,101 @@
+package com.project;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Ciutat implements Serializable {
+    private long ciutatId;
+    private String nom;
+    private String pais;
+    private int poblacio;
+    
+    private Set<Ciutada> ciutadans = new HashSet<>();
+    
+    public Ciutat() {
+        this.ciutadans = new HashSet<>();
+    }
+    
+    public Ciutat(String nom, String pais, int poblacio) {
+        this.nom = nom;
+        this.pais = pais;
+        this.poblacio = poblacio;
+        this.ciutadans = new HashSet<>();
+    }
+
+    public long getCiutatId() {
+        return ciutatId;
+    }
+
+    public void setCiutatId(long ciutatId) {
+        this.ciutatId = ciutatId;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public int getPoblacio() {
+        return poblacio;
+    }
+
+    public void setPoblacio(int poblacio) {
+        this.poblacio = poblacio;
+    }
+
+    public Set<Ciutada> getCiutadans() {
+        return ciutadans;
+    }
+
+    public void setCiutadans(Set<Ciutada> ciutadans) {
+        this.ciutadans = ciutadans;
+    }
+    
+    public void addCiutada(Ciutada ciutada) {
+        ciutadans.add(ciutada);
+        ciutada.setCiutat(this);
+    }
+
+    public void removeCiutada(Ciutada ciutada) {
+        ciutadans.remove(ciutada);
+        ciutada.setCiutat(null);
+    }
+    
+     @Override
+    public String toString() {
+    StringBuilder str = new StringBuilder();
+    for (Ciutada ciutada : ciutadans) {
+        if (str.length() > 0) {
+            str.append(" | ");
+        }
+        str.append(ciutada.getNom());
+    }
+    return this.getCiutatId() + ": " + this.getNom() + ", " + this.getPais() + ", " + this.getPoblacio() + ", Ciutadans: [" + str + "]";
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        
+        Ciutat ciutat = (Ciutat) o;
+        return ciutatId == ciutat.ciutatId;
+    }
+    
+    @Override
+    public int hashCode() {
+        return Long.hashCode(ciutatId);
+    }    
+}
